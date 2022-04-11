a8c2f9cc9f9a008e8f3a5a91ae5fde613b4d13a7
Kaique Tomazini <tomazini.ka@gmail.com>
2022-04-11 16:26:38 -0300

    Created API City and class to interpret the point column

---
+++ src/main/java/com/github/kaiquetomazini/citiesapi/cities/entities/City.java
@@ -0,0 +1,65 @@
+package com.github.kaiquetomazini.citiesapi.cities.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+import org.springframework.data.geo.Point;
+
+@Entity(name = "City")
+@Table(name = "cidade")
+@TypeDefs(value = { 
+	@TypeDef(name = "point", typeClass = PointType.class) 
+})
+public class City {
+
+	@Id
+	private Long id;
+
+	@Column(name = "nome")
+	private String name;
+
+	private Integer uf;
+
+	private Integer ibge;
+
+	@Column(name = "lat_lon")
+	private String geolocation;
+
+	@Type(type = "point")
+	@Column(name = "lat_lon", updatable = false, insertable = false)
+	private Point location;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Integer getUf() {
+		return uf;
+	}
+
+	public Integer getIbge() {
+		return ibge;
+	}
+
+	public String getGeolocation() {
+		return geolocation;
+	}
+
+	public Point getLocation() {
+		return location;
+	}
+
+	public City() {
+
+	}
+
+}
